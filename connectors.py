235e332ce12e42d454e355f43382d046fab1154c
dave <dave@b92750d9-520b-4a69-b44c-71e487b1fbf1>
2014-09-30 21:40:49 +0000

    add connectors.py
    git-svn-id: svn+ssh://sam/home/dave/svn/modules/camcam@742 b92750d9-520b-4a69-b44c-71e487b1fbf1

---
+++ connectors.py
@@ -0,0 +1,47 @@
+class Dconnector(Pathgroup):
+        def __init__(self, pos, **config):
+		self.init(config)
+		self.translate(pos)
+		data={
+			9:{'A':20.5, 'B':10.25, 'C':25, 'D':12.5},
+			15:{'A':28.8, 'B':14.4, 'C':33.3, 'D':16.65},
+			9:{'A':42.5, 'B':21.25, 'C':47, 'D':23.5},
+			9:{'A':59.1, 'B':29.55, 'C':63.5, 'D':31.75},
+		}
+		hole_rad=3.3/2
+		top=5.9
+		h=11.4
+		r=4
+		ex=0.4
+		dw=h*math.tan(10/180*math.pi)
+                self.init(config)
+		if 'pins' in config:
+			if config['pins'] in [9,15,25,37]:
+				pins=config['pins']
+			else:
+				print "D connectors should have 9,15,25, or 37 pins not "+str(config['pins'])
+		else:
+			pins=9
+ 		cutout = self.add(Path(side='in', closed=True))
+		d=data['pins']
+		cutout.add_point(V(-d['B']-ex,top),'incurve',r)
+		cutout.add_point(V(d['B']+ex,top),'incurve',r)
+		cutout.add_point(V(d['B']+ex-dw,top-h),'incurve',r)
+		cutout.add_point(V(-d['B']-ex+dw,top-h),'incurve',r)
+		self.add(Hole(V(d['D'],0),hole_rad))
+		self.add(Hole(V(-d['D'],0),hole_rad))
+
+class XLR(Pathgroup):
+	def __init__(self, pos, **config):
+		self.init(config)
+		self.translate(pos)
+		data={
+			'male':{'h':V(15,0), 'r':22/2},
+			'female':{'h':V(15,5), 'r':25/2},
+		}
+		if 'type' in config:
+			d=data[config['type']
+			self.add(Hole(V(0,0), d['r']))
+			self.add(Hole(d['h'],3.3/2))
+			self.add(Hole(-d['h'],3.3/2))
+		
