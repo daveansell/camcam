b9436063af9a25b71af2537abada3e229ce04c61
Dave Ansell <dave@cambridgesciencecentre.org>
2017-08-23 01:56:35 +0100

    alter subcircle test

--- tests/subcircle/test.py
+++ tests/subcircle/test.py
@@ -1,5 +1,5 @@
 plane = camcam.add_plane(Plane('xy', cutter='1/8_endmill'))
 plane.add_layer('test', material = 'pvc', thickness = 6)
-ratchet=plane.add(Part(name = 'ratchet', layer='test', border=SubCircle(V(0,0), rad = 25, yoff=1)))
+ratchet=plane.add(Part(name = 'ratchet', layer='test', border=Circle(V(0,0), rad = 25, yoff=1)))
 ratchet.add(SubCircle(V(0,0), rad = 18, yoff=6, side='in'))
 
