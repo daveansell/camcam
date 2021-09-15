6186dcdda2b1ceb808915c317cb32f204108547a
Dave Ansell <dave@sciansell.co.uk>
2021-09-16 00:48:43 +0100

    add kicad importer

---
+++ kicad.py
@@ -0,0 +1,140 @@
+#!/usr/bin/env python
+
+import sys
+import math
+from kicad_pcb import *
+from  path import *
+from point import *
+
+class Kicad:
+    def __init__(self, filename):
+        self.pcb = KicadPCB.load(filename)
+# check for error
+        for e in self.pcb.getError():
+            print('Error: {}'.format(e))
+
+    def getModules(self, moduleName):
+        positions=[]
+        for module in self.pcb.module:
+            print (module[0])
+            if module[0]==moduleName:
+                positions.append(self.toV(module['at']))
+        return positions 
+    def toTuple(self, a):
+        return (float(a[0]), float(a[1]))
+    def makeBorders(self, layer):
+        segments=[]
+        # collect all the sections
+        for section in self.pcb.gr_line:
+            if section['layer'] == layer:
+                section['type']='line'
+                newsect={'start':self.toTuple(section['start']), 'end':self.toTuple(section['end']), 'width':section['width'], 'type':'line'}
+                segments.append(newsect)
+        for section in self.pcb.gr_arc:
+            if section['layer'] == layer:
+                newsect={'start':self.toTuple(section['end']), 'centre':self.toTuple(section['start']), 'angle':section['angle'], 'width':section['width']}
+                r = [newsect['start'][0]-newsect['centre'][0], newsect['start'][1]-newsect['centre'][1]]
+                a = section['angle']*math.pi/180
+                end = [newsect['centre'][0]+round(r[0]*math.cos(a) - r[1]*math.sin(a),4), newsect['centre'][1]+round(r[0]*math.sin(a)+r[1]*math.cos(a),4)]
+                newsect['end']=self.toTuple(end)
+                newsect['type']='arc'
+                segments.append(newsect)
+        count=0
+        ends={}
+        # create a hash of all the end positions
+        for c in segments:
+            start= c['start']
+            end = c['end']
+            c['id']=count
+            print ("start="+str(c['start'])+ "end="+str(c['end'])+" type="+str(c['type']))
+            if start in ends:
+                ends[start].append([count,'start'])
+            else:
+                ends[start]=[[count,'start']]
+            if end in ends:
+                ends[end].append([count,'end'])
+            else:
+                ends[end]=[[count,'end']]
+            count+=1
+        loops=[]
+        for s in range(0, len(segments)):
+            if 'reversed' not in segments[s]:
+                loops.append(self.getLoop(ends, segments, s,s, 'start', 'start'))
+        paths=[]
+        for loop in loops:
+            paths.append(self.makePath(loop))
+
+        for section in self.pcb.gr_circle:
+            if section['layer'] == layer:
+                section['type']='arc'
+                paths.append(Circle(
+                    self.toV(section['start']),
+                    rad=(self.toV(section['start'])-self.toV(section['end'])).length()
+                    )
+                )
+        if layer == 'Edge.Cuts':
+            for i in range(0,len(paths)):
+                fail=False
+                for j in range(0, len(paths)):
+                    if i!=j:
+                        if paths[i].contains(paths[j])!=1:
+                            fail=True
+                            break
+                        else:
+                            paths[j].side='in'
+                if not fail:
+                    print (paths)
+                    print(paths[i])
+                    self.border=paths[i]
+                    paths.remove(paths[i])
+                    self.paths=paths
+                    self.border.side='out'
+        else:
+            return paths
+
+    def toV(self, t):
+        return V(t[0], t[1])
+
+    def makePath(self, loop):
+        path = Path(closed=True)
+        for l in loop:
+            if l['type']=='line':
+                print(l['end'])
+                path.add_point(PSharp(self.toV(l['end'])))
+            if l['type']=='arc':
+                if l['reversed']:
+                    direction='cw'
+                else:
+                    direction='ccw'
+                rad=(self.toV(l['start'])-self.toV(l['centre'])).length()
+                path.add_point(PArc(self.toV(l['centre']), radius=rad, direction=direction))
+                path.add_point(PSharp(self.toV(l['end'])))
+        return path
+    def getLoop(self, ends, segments, p, start, i, istart, depth=0):
+        # we have gone all the way around the loop
+        if(p==start and depth!=0):
+            return []
+        segment=segments[p]
+        pos = segment[i]
+        end = ends[pos]
+        if end[0][0]==p:
+            a=1
+            na=0
+        else:
+            a=0
+            na=1
+        if end[a][1]=='start':
+            newi='end'
+        else:
+            newi='start'
+
+        loop=self.getLoop(ends,segments, end[a][0], start, newi, istart, depth+1 )
+        if(end[na][1]=='start'):
+            segment['reversed']=False
+        else:
+            segment['reversed']=True
+        loop.insert(0,segment) 
+        return loop
+
+
+
