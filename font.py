717fa5ece9e45b45a983fcf2b3bee3f55bb1537a
Dave Ansell <dave@sciansell.co.uk>
2022-10-27 00:27:33 +0100

    add curvedHersheyText

--- font.py
+++ font.py
@@ -202,6 +202,50 @@ class Text(Pathgroup):
         out2.orig_direction = out.find_direction({})
         return out2
 
+class CurvedHersheyText(Pathgroup):
+    def __init__(self, pos, rad, text, **config):
+        self.init(config)
+        thefont = HersheyFonts()
+        if 'font' in config:
+            thefont.load_default_font(config['font'])
+        else:
+            thefont.load_default_font('rowmans')
+        if 'scale' in config:
+            self.scale =float(config['scale'])
+        else:
+            self.scale=1.0
+        if 'dir' in config:
+            if config['dir'] == 'cw':
+                direction = 1
+            elif config['dir'] == 'ccw':
+                direction = -1
+            else:
+                print("CurvedText direction "+str(config['dir'])+" should be 'cw' or 'ccw'")
+        else:
+            direction = 1
+        lastx=None
+        lasty=None
+        minx = 100000
+        miny = 100000
+        maxx = -100000
+        maxy = -100000
+        a = 0
+        thepath = False
+        for (x1, y1), (x2, y2) in thefont.lines_for_text(text):
+            if lastx!=x1 or lasty!=y1:
+                thepath = self.add(Path(closed=False))
+                thepath.add_point(rotate(V(0,rad-y1*self.scale), x1*self.scale/rad/math.pi *180.0))
+        #    print(str(V(0,rad-y2*self.scale))+ " - " + str(x1*self.scale/rad/math.pi *180.0) + " " + str(rotate(V(0,rad-y2*self.scale), x1*self.scale/rad/math.pi *180.0)))
+            thepath.add_point(rotate(V(0,rad-y2*self.scale), x2*self.scale/rad/math.pi *180.0))
+            lastx=x2
+            lasty=y2
+            maxx = max(maxx, x1, x2)
+            maxy = max(maxy, y1, y2)
+            minx = min(minx, x1, x2)
+            miny = min(miny, y1, y2)
+        if 'centred' in config and config['centred']:
+            self.rotate(V(0,0), -(maxx-minx)*self.scale/rad/math.pi*180/2)
+        self.translate(pos)
 class CurvedText(Text):
     def __init__(self, pos, rad, text, **config):
         self.init(config)
