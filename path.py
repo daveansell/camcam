d8576676f5985232e28909e80ec12e0572e08974
Dave Ansell <dave@sciansell.co.uk>
2023-05-30 12:05:26 +0100

    alter spindlerpm code

--- path.py
+++ path.py
@@ -293,10 +293,13 @@ class Path(object):
         if('material' in config and config['material'] is not None):
             mat=milling.materials[config['material']]
             if 'spindleRPM' not in config or config['spindleRPM'] is None:
-                    config['spindleRPM']= mat['surface_speed']/config['cutterrad']/math.pi/2
+                    if config['cutterrad']==0:
+                        config['spindleRPM']= mat['surface_speed']/2/math.pi/2
+                    else:
+                        config['spindleRPM']= mat['surface_speed']/config['cutterrad']/math.pi/2
             if 'sidefeed' not in config or config['sidefeed'] is None and 'type' not in tool or tool['type'] !='lathe':
                 #    config['sidefeed']=mat['sidefeed']
-                print("chip loading ="+str(self.get_chip_loading(config['cutterrad'], mat['chip_loading']['low']) ))
+               # print("chip loading ="+str(self.get_chip_loading(config['cutterrad'], mat['chip_loading']['low']) ))
                 config['sidefeed']= self.get_chip_loading(config['cutterrad'], mat['chip_loading']['low']) * config['spindleRPM']*tool['flutes']
         if 'stepdown' in config and config['stepdown'] and 'sidefeed' in config and config['sidefeed'] and config['stepdown']>1.5*tool['diameter']:
             config['sidefeed']*= 1.5*tool['diameter']/config['stepdown']
