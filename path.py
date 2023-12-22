8f69fd9a26e550e81ca84f3ac5c551d4360cfb40
Dave Ansell <dave@sciansell.co.uk>
2023-12-22 12:28:34 +0000

    fix circle intersect circle

--- path.py
+++ path.py
@@ -508,7 +508,7 @@ class Path(object):
     def ccw(self, a, b, c):
         return (b[0] - a[0]) * (c[1] - a[1]) - (c[0] - a[0]) * (b[1] - a[1]);
 
-    def circleIntersectsCircle(c1, r1, c2,r2):
+    def circleIntersectsCircle(self,c1, r1, c2,r2):
         R = (c1-c2).length()
         b= 0.5*(c1+c2)+(r1**2-r2**2)/2/R**2*(c2-c1)
         c= 0.5*math.sqrt( 2*(r1**2+r2**2)/R**2-(r1**2-r2**2)**2/R**4 -1)*V(c2[1]-c1[1], c1[0]-c2[0])
