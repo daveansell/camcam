567dbab7a3fc42dfc31c5ceef65d4a7d773873a4
Dave Ansell <dave@sciansell.co.uk>
2024-04-05 11:10:29 +0100

    acetal cut tweak

--- Milling.py
+++ Milling.py
@@ -540,6 +540,8 @@ class Milling:
                         "endcut":1,
                         "sidecut":1,
                         "flutes":1,
+			"sidefeed":1,
+			"vertfeed":1,
                 },
                 "latheBoring":{
                         "id":50,
@@ -634,7 +636,7 @@ class Milling:
                         "sidefeed":1000,
                         "stepdown":3.0,
                         "kress_setting":2.0,
-                        "mill_dir":'up',
+                        "mill_dir":'down',
                         "surface_speed":500*300, # mm/min
                         "chip_loading":{
                             'low':{3.16:	0.0508	,6.32:	0.1524	,9.48:	0.2032	,12.64:	0.254},
